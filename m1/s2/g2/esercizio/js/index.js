8170161666082288885fffbc84b6a99253ce1a67
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-04-04 16:57:03 +0200

    done

--- m1/s2/g2/esercizio/js/index.js
+++ m1/s2/g2/esercizio/js/index.js
@@ -7,7 +7,7 @@ titleExercise.innerHTML = "<h1>Esercizio Giorno 2, Settimana 2</h1>";
 
 titleExercise.setAttribute("style", "background-color: yellow; font-weight: bold; font-size: 3rem; text-align: center; padding: 3% 0 3% 0");
 
-content.innerHTML = "<h2 id=\"exercise\">Rispondi alle seguenti domande:</h2>\n<ol id=\"questions\"><li id=\"question1\"></li><li id=\"question2\"></li><li id=\"question3\"></li><li id=\"question4\"></li><li id=\"question5\"></li><li id=\"question6\"></li><li id=\"question7\"></li></ol>"; 
+content.innerHTML = "<h2 id=\"exercise\">Rispondi alle seguenti domande:</h2>\n<ol start=\"1\" id=\"questions\"><li id=\"question1\"></li><li id=\"question2\"></li><li id=\"question3\"></li><li id=\"question4\"></li><li id=\"question5\"></li><li id=\"question6\"></li><li id=\"question7\"></li></ol>"; 
 
 let liElements = document.querySelectorAll("#questions li");
 
@@ -18,20 +18,20 @@ liElements.forEach(function(li) {
 let titleH2 = document.getElementById('exercise');
 
 titleH2.setAttribute("style", "text-align: center; font-weight: bold; font-size: 2rem; padding: 3% 0 3% 0; color: green");
-
+/*
 let questionsList = document.getElementsByClassName('questions');
 
 questionsList.setAttribute("style", "text-align: start; font-size: 1.5rem; padding: 1.5% 1% 3% 0; color: green; text-decoration: italic");
-/*
-let question_1 = document.getElementsById('question1');
-let question_2 = document.getElementsById('question2');
-let question_3 = document.getElementsById('question3');
-let question_4 = document.getElementsById('question4');
-let question_5 = document.getElementsById('question5');
-let question_6 = document.getElementsById('question6');
-let question_7 = document.getElementsById('question7');
+*/
+let question_1 = document.getElementById('question1');
+let question_2 = document.getElementById('question2');
+let question_3 = document.getElementById('question3');
+let question_4 = document.getElementById('question4');
+let question_5 = document.getElementById('question5');
+let question_6 = document.getElementById('question6');
+let question_7 = document.getElementById('question7');
 
-/*question_1.innerHTML = "Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino."
+question_1.innerHTML = "Elenca e descrivi i principali datatype in JavaScript. Prova a spiegarli come se volessi farli comprendere a un bambino."
 
 question_2.innerHTML = "Crea una variable chiamata \"name\" e assegna ad essa il tuo nome, sotto forma di stringa."
 
@@ -41,6 +41,15 @@ question_4.innerHTML = "Crea una variable di nome \"x\" e assegna ad essa il num
 
 question_5.innerHTML = "Riassegna un nuovo valore alla variabile \"name\" già esistente: il tuo cognome. Dimostra l'impossibilità di riassegnare un valore ad una variabile dichiarata con il costrutto const."
 
-question_6.innerHTML = "Esegui una sottrazione tra i numeri 4 e la variable "x" appena dichiarata (che contiene il numero 12)."
+question_6.innerHTML = "Esegui una sottrazione tra i numeri 4 e la variable \"x\" appena dichiarata (che contiene il numero 12)."
 
 question_7.innerHTML = "Crea due variabili: \"name1\" e \"name2\". Assegna a name1 la stringa \"john\", e assegna a name2 la stringa \"John\" (con la J maiuscola!).\nVerifica che name1 sia diversa da name2 (suggerimento: è la stessa cosa di verificare che la loro uguaglianza sia falsa)."
+
+let buttons = document.querySelectorAll('.buttons');
+
+buttons.forEach(function(button, index) {
+  button.addEventListener('click', function() {
+    let page = 'pagina' + (index + 1) + '.html';
+    window.location.href = page;
+  });
+});
