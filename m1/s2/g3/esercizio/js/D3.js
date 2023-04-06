a8aaad53af7fcd044a43a933398ebfbdf7d25e75
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-04-06 17:03:53 +0200

    esercizio g4

--- m1/s2/g3/esercizio/js/D3.js
+++ m1/s2/g3/esercizio/js/D3.js
@@ -236,7 +236,7 @@ let result = isOddEven(20);
 */
 
 /* SCRIVI QUI LA TUA RISPOSTA */
-
+{
 let val = 7
   if (val < 5) {
       console.log("Meno di 5");
@@ -245,7 +245,27 @@ let val = 7
     } else {
       console.log("Uguale a 10 o maggiore");
     }
-
+}
+{
+  let val = 4
+    if (val < 5) {
+        console.log("Meno di 5");
+      } else if (val < 10) {
+        console.log("Meno di 10");
+      } else {
+        console.log("Uguale a 10 o maggiore");
+      }
+}
+{
+  let val = 11
+    if (val < 5) {
+        console.log("Meno di 5");
+      } else if (val < 10) {
+        console.log("Meno di 10");
+      } else {
+        console.log("Uguale a 10 o maggiore");
+      }
+}
 /* ESERCIZIO 11
   Fornito il seguente oggetto, scrivi del codice per aggiungere una proprietà "city", il cui valore sarà "Toronto".
 */
