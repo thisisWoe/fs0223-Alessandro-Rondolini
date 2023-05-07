8277a1fb746b737fbc53d8019bbb8bb124511442
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-05-07 21:29:51 +0200

    hero section responsive

--- m2/s2/g5 - compito/netflix/Home Page/script/script.js
+++ m2/s2/g5 - compito/netflix/Home Page/script/script.js
@@ -1,29 +1,23 @@
+// CAROUSELS ANIMATIONS
+let allCarousels = document.querySelectorAll('.carousel');
+console.log(allCarousels);
 
-/* let imgCarousel = document.getElementById('get-height');
-console.log(imgCarousel);
-imgCarousel.addEventListener('load', function() {
-    // ottieni l'altezza dell'immagine
-    var altezza = imgCarousel.getBoundingClientRect().height;
-    
-    // fai qualcosa con l'altezza (es. assegnala ad un altro elemento)
-    console.log(altezza);
-  }); */
-/* let getHeight = imgCarousel.getBoundingClientRect().height;
-console.log(getHeight); */
-/* let allCarouselButtons = document.querySelectorAll('.usr-selcet-nn');
-console.log(allCarouselButtons);
-for (let i = 0; i < allCarouselButtons.length; i++) {
-    allCarouselButtons.style.height = getHeight + 'px';
-} */
-
-
-/* let carouselImgs = document.querySelectorAll('.img-fluid');
-console.log(carouselImgs);
-
-carouselImgs.forEach(function (image){
-    if (window.innerWidth <= 768){
-        image.style['margin-top'] = '0';
-        image.style['margin-bottom'] = '0';
+for (let i = 0; i < allCarousels.length; i++) {
+    let singleCarousel = allCarousels[i]
+    if (singleCarousel) {
+      let randomAnimation = Math.floor(Math.random() * 5000) + 5000;
+      setInterval(function() {
+        singleCarousel.querySelector('.carousel-control-next').click();
+      }, randomAnimation);
     }
 }
-) */
+
+
+//MODAL OPEN
+/* const myModal = document.getElementById('myModal') */
+const myButton = document.getElementById('myInput');
+
+myButton.addEventListener('click', () => {
+  const modal = new bootstrap.Modal(myModal);
+  modal.toggle();
+});
