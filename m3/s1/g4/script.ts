d58c3df67f5d8c5438b3772819a473fd5cd92700
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-05-25 16:36:28 +0200

    esercizio

--- m3/s1/g4/script.ts
+++ m3/s1/g4/script.ts
@@ -1,7 +1,37 @@
-/* import {myModule} from '../../../myModules/myModules'; */
+/* import { myModule } from '../../../myModules/myModules';
+console.log("🚀 ~ file: script.ts:2 ~ myModule:", myModule)
+
+let body = Q('body');
+console.log("🚀 ~ file: script.ts:4 ~ body:", body) */
 
 
-/* class CapoAbbigliamento  */
+abstract class CapoAbbigliamento {
+    capo: string;
+    codprod: number;
+    collezione: string;
+    colore: string;
+    disponibile: string;
+    id: number;
+    modello: number;
+    prezzoivaesclusa: number;
+    prezzoivainclusa: number;
+    quantita: number;
+    saldo: number;
+    constructor(capo: string, codprod: number, collezione: string, colore: string, disponibile: string, id: number, modello: number, prezzoivaesclusa: number, prezzoivainclusa: number, quantita: number, saldo: number){
+        this.capo = capo;
+        this.codprod =  codprod;
+        this.collezione = collezione;
+        this.colore = colore;
+        this.disponibile = disponibile;
+        this.id = id;
+        this.modello = modello;
+        this.prezzoivaesclusa = prezzoivaesclusa;
+        this.prezzoivainclusa = prezzoivainclusa;
+        this.quantita = quantita;
+        this.saldo= saldo;
+    }
+    abstract mostraPrezzo():void;
+}
 
 
 
@@ -19,5 +49,40 @@ fetch('./Abbigliamento.json')
 })
 .then(data => {
   console.log("🚀 ~ file: script.ts:15 ~ data:", data)
-  
+  let capiAbbigliamento:CapoAbbigliamento[] = [];
+
+  class NewCapo extends CapoAbbigliamento {
+    prezzoScontato: string;
+    constructor(capo: string, codprod: number, collezione: string, colore: string, disponibile: string, id: number, modello: number, prezzoivaesclusa: number, prezzoivainclusa: number, quantita: number, saldo: number){
+        super(capo, codprod, collezione, colore, disponibile, id, modello, prezzoivaesclusa, prezzoivainclusa, quantita, saldo)
+        this.prezzoScontato = this.mostraPrezzo()
+    }
+    mostraPrezzo():string {
+        let prezzoScontato: number = (this.prezzoivainclusa / 100) * this.saldo;
+        return `Il prezzo scontato del capo è ${(this.prezzoivainclusa - prezzoScontato).toFixed(2)}€`;
+    }
+  }
+
+  data.forEach((capoSingolo: NewCapo) => {
+        let newCapoSingolo = new NewCapo(
+            capoSingolo.capo,
+            capoSingolo.codprod,
+            capoSingolo.collezione,
+            capoSingolo.colore,
+            capoSingolo.disponibile,
+            capoSingolo.id,
+            capoSingolo.modello,
+            capoSingolo.prezzoivaesclusa,
+            capoSingolo.prezzoivainclusa,
+            capoSingolo.quantita,
+            capoSingolo.saldo
+        )
+        capiAbbigliamento.push(newCapoSingolo);
+    })
+    console.log("🚀 ~ file: script.ts:68 ~ capiAbbigliamento.forEach ~ capiAbbigliamento:", capiAbbigliamento)
+    
+    const numeriPari:number[] = [0, 2, 4]
+    numeriPari.forEach((num) => {
+        console.log(capiAbbigliamento[num].prezzoScontato)
+    })
 })
