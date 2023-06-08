6fc29e9d9aa91b2e090caf3ae0c12ee245d5d0be
thisisWoe <alessandro.rondolini.96@gmail.com>
2023-06-08 09:54:41 +0200

    fix

--- m3/s3/g3/progetto/src/app/app-routing.module.ts
+++ m3/s3/g3/progetto/src/app/app-routing.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
-  { path: 'home', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
+  { path: '', loadChildren: () => import('./pages/home/home.module').then(m => m.HomeModule) },
   { path: 'todo', loadChildren: () => import('./pages/todo/todo.module').then(m => m.TodoModule) },
   { path: 'completed', loadChildren: () => import('./pages/completed/completed.module').then(m => m.CompletedModule) },
   { path: 'register', loadChildren: () => import('./pages/auth/register/register.module').then(m => m.RegisterModule) },
--- m3/s3/g3/progetto/src/app/pages/auth/register/register-routing.module.ts
+++ m3/s3/g3/progetto/src/app/pages/auth/register/register-routing.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
-import { RegisterComponent } from '../../register/register.component';
+import { RegisterComponent } from './register.component';
 
 const routes: Routes = [{ path: '', component: RegisterComponent }];
 
